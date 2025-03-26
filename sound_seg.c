5971f614d4abf4eb98969c01fcaa3b160786b009
Neil Narnoli <nnar0302@uni.sydney.edu.au>
2025-03-26 13:15:32 +1100

    Modified functions tr_delete_range

--- sound_seg.c
+++ sound_seg.c
@@ -85,11 +85,11 @@ static void append_new_node(struct sound_seg* track, const int16_t* src, size_t 
 
 // Read len elements from position pos into dest
 void tr_read(struct sound_seg* track, int16_t* dest, size_t pos, size_t len) {
-    if(!track || !dest || len == 0) return 0;
+    if(!track || !dest || len == 0) return;
 
     seg_node* current = track->head;
     size_t i = 0;
-    size_t remaining = 0;
+    size_t remaining = len;
     size_t write_idx = 0;
 
     while ((current) && (remaining > 0)){
@@ -111,9 +111,9 @@ void tr_read(struct sound_seg* track, int16_t* dest, size_t pos, size_t len) {
 
             memcpy(&dest[write_idx], &current->data[start_in_node], can_read * sizeof(int16_t));
 
-            write_idx   += can_read;
-            remaining   -= can_read;
-            pos         += can_read;
+            write_idx += can_read;
+            remaining -= can_read;
+            pos += can_read;
         }
 
         i += current->length;
@@ -133,32 +133,30 @@ void tr_write(struct sound_seg* track, int16_t* src, size_t pos, size_t len) {
         append_new_node(track, src, len);
         return;
     }
-    
-    seg_node* cur = track->head;
-    size_t offset = 0;
+
+    seg_node* current = track->head;
+    size_t i = 0;
     size_t written = 0;
     size_t to_write = len;
 
-    while (cur && to_write > 0) {
-        size_t node_start = offset;
-        size_t node_end   = offset + cur->length;
+    while ((current) && (to_write > 0)) {
+        size_t start = i;
+        size_t end   = i + current->length;
 
-        if (pos < node_end) {
-            size_t start_in_node = pos - node_start;
-            size_t can_write = cur->length - start_in_node;
+        if (pos < end) {
+            size_t start_in_node = pos - start;
+            size_t can_write = current->length - start_in_node;
             if (can_write > to_write) {
                 can_write = to_write;
             }
-            memcpy(&cur->data[start_in_node],
-                   &src[written],
-                   can_write * sizeof(int16_t));
+            memcpy(&current->data[start_in_node], &src[written], can_write * sizeof(int16_t));
 
             written += can_write;
             to_write -= can_write;
-            pos      += can_write;
+            pos += can_write;
         }
-        offset += cur->length;
-        cur = cur->next;
+        i += current->length;
+        current = current->next;
     }
 
     if (to_write > 0) {
@@ -168,6 +166,108 @@ void tr_write(struct sound_seg* track, int16_t* src, size_t pos, size_t len) {
 
 // Delete a range of elements from the track
 bool tr_delete_range(struct sound_seg* track, size_t pos, size_t len) {
+    if (!track || len == 0) return true;
+    if (pos >= track->total_length) {
+        return true;
+    }
+
+    size_t end = pos + len;
+    if (end > track->total_length) {
+        end = track->total_length;
+    }
+    size_t range_len = end - pos;
+
+    seg_node dummy;
+    dummy.next = track->head;
+    seg_node* previous = &dummy;
+    seg_node* current  = track->head;
+    size_t i = 0;
+
+    while ((current) && (i < end)) {
+        size_t node_start = i;
+        size_t node_end   = i + current->length;
+
+        if (node_end <= pos) {
+            i = node_end;
+            previous = current;
+            current  = current->next;
+        }
+        else if (node_start >= end) {
+            break;
+        }
+        else {
+            size_t overlap_start = pos;
+            if (node_start > pos) {
+                overlap_start = node_start;
+            }
+
+            size_t overlap_end = end;
+            if (node_end < end) {
+                overlap_end = node_end;
+            }
+            size_t overlap_len   = overlap_end - overlap_start;
+
+            // number of samples before deletion starts
+            size_t node_offset_start = overlap_start - node_start;
+            // number of samples from the start of the node till the deletion ends
+            size_t node_offset_end   = overlap_end   - node_start;
+
+            if (overlap_len == current->length) {
+                previous->next = current->next;
+                seg_node* temp = current;
+                current = current->next;
+                i = node_end;
+                track->total_length -= overlap_len;
+
+                free(temp->data);
+                free(temp);
+            } else {
+                int16_t* original = current->data;
+
+                size_t left_len  = node_offset_start;
+                size_t right_len = current->length - node_offset_end;
+
+                if ((left_len > 0) && (right_len > 0)) {
+                    seg_node* right_node = malloc(sizeof(seg_node));
+                    right_node->data = malloc(right_len * sizeof(int16_t));
+                    memcpy(right_node->data, &original[node_offset_end], right_len * sizeof(int16_t));
+                    right_node->length = right_len;
+                    right_node->next = current->next;
+
+                    int16_t* left_data = malloc(left_len * sizeof(int16_t));
+                    memcpy(left_data, original, left_len * sizeof(int16_t));
+                    free(original);
+
+                    current->data = left_data;
+                    current->length = left_len;
+                    current->next = right_node;
+
+                } else if (left_len == 0) {
+                    int16_t* right_data = malloc(right_len * sizeof(int16_t));
+                    memcpy(right_data, &original[node_offset_end], right_len * sizeof(int16_t));
+                    free(original);
+
+                    current->data   = right_data;
+                    current->length = right_len;
+                } else {
+                    // right_len == 0
+                    int16_t* left_data = malloc(left_len * sizeof(int16_t));
+                    memcpy(left_data, original, left_len * sizeof(int16_t));
+                    free(original);
+
+                    current->data   = left_data;
+                    current->length = left_len;
+                }
+
+                track->total_length -= overlap_len;
+                i = node_start + current->length; 
+                previous = current;
+                current = current->next;
+            }
+        }
+    }
+
+    track->head = dummy.next;
     return true;
 }
 
